d94287044ee5d669d1cd362ae7beb8930886b416
腹黒い茶 <haraguroicha@tipsy.coffee>
2018-06-05 13:59:56 +0800

    freshen test

--- main_test.go
+++ main_test.go
@@ -61,8 +61,9 @@ func TestGetEmptyTopics(t *testing.T) {
 
 // trying to add topic 1
 func TestNewTopic1(t *testing.T) {
+	topicTitle := "topic 1"
 	testHTTPResponse(HTTPPost("/api/newTopic", &Topic.RequestOfTopic{
-		TopicTitle: "topic 1",
+		TopicTitle: topicTitle,
 	}), func(w *httptest.ResponseRecorder) {
 		assert.Equal(t, http.StatusOK, w.Code)
 
@@ -71,13 +72,15 @@ func TestNewTopic1(t *testing.T) {
 
 		assert.Equal(t, true, responsed.Success)
 		assert.Equal(t, 1, len(responsed.Data))
+		assert.Equal(t, topicTitle, responsed.Data[0].TopicTitle)
 	})
 }
 
 // trying to add topic 2
 func TestNewTopic2(t *testing.T) {
+	topicTitle := "topic 2"
 	testHTTPResponse(HTTPPost("/api/newTopic", &Topic.RequestOfTopic{
-		TopicTitle: "topic 2",
+		TopicTitle: topicTitle,
 	}), func(w *httptest.ResponseRecorder) {
 		assert.Equal(t, http.StatusOK, w.Code)
 
@@ -86,13 +89,15 @@ func TestNewTopic2(t *testing.T) {
 
 		assert.Equal(t, true, responsed.Success)
 		assert.Equal(t, 2, len(responsed.Data))
+		assert.Equal(t, topicTitle, responsed.Data[1].TopicTitle)
 	})
 }
 
 // trying to add a long topic and just before the length of exceed length
 func TestNewTopicLong(t *testing.T) {
+	topicTitle := "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345"
 	testHTTPResponse(HTTPPost("/api/newTopic", &Topic.RequestOfTopic{
-		TopicTitle: "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345",
+		TopicTitle: topicTitle,
 	}), func(w *httptest.ResponseRecorder) {
 		assert.Equal(t, http.StatusOK, w.Code)
 
@@ -101,13 +106,15 @@ func TestNewTopicLong(t *testing.T) {
 
 		assert.Equal(t, true, responsed.Success)
 		assert.Equal(t, 3, len(responsed.Data))
+		assert.Equal(t, topicTitle, responsed.Data[2].TopicTitle)
 	})
 }
 
 // trying to add a long topic and it was exceed of the maximum length
 func TestNewTopicLongAndWontSuccess(t *testing.T) {
+	topicTitle := "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456"
 	testHTTPResponse(HTTPPost("/api/newTopic", &Topic.RequestOfTopic{
-		TopicTitle: "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456",
+		TopicTitle: topicTitle,
 	}), func(w *httptest.ResponseRecorder) {
 		assert.NotEqual(t, http.StatusOK, w.Code)
 
@@ -121,8 +128,9 @@ func TestNewTopicLongAndWontSuccess(t *testing.T) {
 
 // trying to add a normal length topic and it should success
 func TestNewTopicAsNormalAgain(t *testing.T) {
+	topicTitle := "test again"
 	testHTTPResponse(HTTPPost("/api/newTopic", &Topic.RequestOfTopic{
-		TopicTitle: "test again",
+		TopicTitle: topicTitle,
 	}), func(w *httptest.ResponseRecorder) {
 		assert.Equal(t, http.StatusOK, w.Code)
 
@@ -131,15 +139,16 @@ func TestNewTopicAsNormalAgain(t *testing.T) {
 
 		assert.Equal(t, true, responsed.Success)
 		assert.Equal(t, 4, len(responsed.Data))
+		assert.Equal(t, topicTitle, responsed.Data[3].TopicTitle)
 	})
 }
 
-// trying to add manys topics and can not be responsed count greater than the paging maximum
-func TestInsertManysTopics(t *testing.T) {
+func addManysTopic(t *testing.T, startAt int, additionTest func(*testing.T, string, *QueryResponse)) {
 	totalTopicsToAdd := Configs.Config.TopicsPerPage + 5
 	for i := 1; i <= totalTopicsToAdd; i++ {
+		topicTitle := fmt.Sprintf("manys-topic-%d", i+startAt)
 		testHTTPResponse(HTTPPost("/api/newTopic", &Topic.RequestOfTopic{
-			TopicTitle: fmt.Sprintf("manys-topic-%d", i),
+			TopicTitle: topicTitle,
 		}), func(w *httptest.ResponseRecorder) {
 			assert.Equal(t, http.StatusOK, w.Code)
 
@@ -147,9 +156,18 @@ func TestInsertManysTopics(t *testing.T) {
 			json.Unmarshal(w.Body.Bytes(), &responsed)
 
 			assert.Equal(t, true, responsed.Success)
-			assert.Equal(t, math.Min(float64(Configs.Config.TopicsPerPage), float64(4+i)), float64(len(responsed.Data)))
+			assert.Equal(t, math.Min(float64(Configs.Config.TopicsPerPage), float64(4+i+startAt)), float64(len(responsed.Data)))
+			additionTest(t, topicTitle, responsed)
+			t.Log("TopicIDs", Topic.GetTopicIDs(topics))
 		})
 	}
+}
+
+// trying to add manys topics and can not be responsed count greater than the paging maximum
+func TestInsertManysTopics(t *testing.T) {
+	addManysTopic(t, 0, func(t *testing.T, topicTitle string, responsed *QueryResponse) {
+		assert.Equal(t, topicTitle, topics[len(topics)-1].TopicTitle)
+	})
 }
 
 // trying to UpVote some topics
@@ -165,6 +183,7 @@ func TestUpVotes(t *testing.T) {
 		assert.Equal(t, []int{3, 0}, _topicIDs[0:2])
 		assert.Equal(t, &Topic.Votes{UpVotes: 1, DownVotes: 0, SumVotes: 1}, responsed.Data[0].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 0, DownVotes: 0, SumVotes: 0}, responsed.Data[1].Votes)
+		t.Log("TopicIDs", Topic.GetTopicIDs(topics))
 	})
 
 	testHTTPResponse(HTTPPost("/api/upVote/4", nil), func(w *httptest.ResponseRecorder) {
@@ -179,6 +198,7 @@ func TestUpVotes(t *testing.T) {
 		assert.Equal(t, &Topic.Votes{UpVotes: 1, DownVotes: 0, SumVotes: 1}, responsed.Data[0].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 1, DownVotes: 0, SumVotes: 1}, responsed.Data[1].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 0, DownVotes: 0, SumVotes: 0}, responsed.Data[2].Votes)
+		t.Log("TopicIDs", Topic.GetTopicIDs(topics))
 	})
 
 	testHTTPResponse(HTTPPost("/api/upVote/4", nil), func(w *httptest.ResponseRecorder) {
@@ -193,6 +213,14 @@ func TestUpVotes(t *testing.T) {
 		assert.Equal(t, &Topic.Votes{UpVotes: 2, DownVotes: 0, SumVotes: 2}, responsed.Data[0].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 1, DownVotes: 0, SumVotes: 1}, responsed.Data[1].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 0, DownVotes: 0, SumVotes: 0}, responsed.Data[2].Votes)
+		t.Log("TopicIDs", Topic.GetTopicIDs(topics))
+	})
+}
+
+// trying to add manys topics and can not be responsed count greater than the paging maximum
+func TestInsertManysTopicsAgain(t *testing.T) {
+	addManysTopic(t, 25, func(t *testing.T, topicTitle string, responsed *QueryResponse) {
+		assert.Equal(t, topicTitle, topics[len(topics)-1].TopicTitle)
 	})
 }
 
@@ -210,6 +238,7 @@ func TestDownVotes(t *testing.T) {
 		assert.Equal(t, &Topic.Votes{UpVotes: 2, DownVotes: 1, SumVotes: 1}, responsed.Data[0].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 1, DownVotes: 0, SumVotes: 1}, responsed.Data[1].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 0, DownVotes: 0, SumVotes: 0}, responsed.Data[2].Votes)
+		t.Log("TopicIDs", Topic.GetTopicIDs(topics))
 	})
 
 	testHTTPResponse(HTTPPost("/api/downVote/4", nil), func(w *httptest.ResponseRecorder) {
@@ -224,6 +253,7 @@ func TestDownVotes(t *testing.T) {
 		assert.Equal(t, &Topic.Votes{UpVotes: 1, DownVotes: 0, SumVotes: 1}, responsed.Data[0].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 2, DownVotes: 2, SumVotes: 0}, responsed.Data[1].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 0, DownVotes: 0, SumVotes: 0}, responsed.Data[2].Votes)
+		t.Log("TopicIDs", Topic.GetTopicIDs(topics))
 	})
 
 	testHTTPResponse(HTTPPost("/api/downVote/4", nil), func(w *httptest.ResponseRecorder) {
@@ -239,5 +269,13 @@ func TestDownVotes(t *testing.T) {
 		assert.Equal(t, &Topic.Votes{UpVotes: 1, DownVotes: 0, SumVotes: 1}, responsed.Data[0].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 0, DownVotes: 0, SumVotes: 0}, responsed.Data[1].Votes)
 		assert.Equal(t, &Topic.Votes{UpVotes: 2, DownVotes: 3, SumVotes: -1}, topics[len(topics)-1].Votes)
+		t.Log("TopicIDs", Topic.GetTopicIDs(topics))
+	})
+}
+
+// trying to add manys topics and can not be responsed count greater than the paging maximum
+func TestInsertManysTopicsAgainAndAgain(t *testing.T) {
+	addManysTopic(t, 50, func(t *testing.T, topicTitle string, responsed *QueryResponse) {
+		assert.Equal(t, topicTitle, topics[len(topics)-2].TopicTitle) // it should be at last two to find last we added topic
 	})
 }
